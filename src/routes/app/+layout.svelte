c0fd1862cddddab202b133381a41120bd9799f7e
Tomas Vergara <tominacho@hotmail.com>
2024-11-20 16:11:11 -0300

    added toast support

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
 	import Sidebar from './(components)/Sidebar.svelte'; // Sidebar común
 	import Header from './(components)/Header.svelte'; // Header común
+	import { Toaster } from '$lib/components/ui/sonner';
 
 	const { children, data } = $props();
-	$inspect(data.session);
 </script>
 
 <div class="flex min-h-screen w-full flex-col bg-muted/40">
+	<Toaster />
 	<Sidebar />
 	<!-- Sidebar común para todas las páginas -->
 
 	<div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
 		<Header userAvatarUrl={data.session?.user.user_metadata.avatar_url} />
 		<!-- Header común para todas las páginas -->
-		<main>
+		<main class="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
 			{@render children()}
 			<!-- El contenido de cada página se inyecta aquí -->
 		</main>
