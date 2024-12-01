5706f7eaac2176fd486baae15f800565564fb4a0
Tomas Vergara <tominacho@hotmail.com>
2024-12-01 16:24:36 -0300

    feat: add ImageWithLoader component for improved image loading experience

---
+++ src/lib/components/custom/ImageWithLoader.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let src;
+	export let alt = '';
+	let isLoaded = false;
+
+	const handleLoad = () => {
+		isLoaded = true;
+	};
+</script>
+
+<div class="relative">
+	<!-- Skeleton loader -->
+	<div
+		class="absolute inset-0 animate-pulse bg-zinc-200 dark:!bg-secondary"
+		class:hidden={isLoaded}
+	></div>
+
+	<!-- Imagen -->
+	<img
+		{src}
+		{alt}
+		class="h-auto w-full object-contain opacity-0 transition-opacity duration-500"
+		on:load={handleLoad}
+		class:opacity-100={isLoaded}
+	/>
+</div>
