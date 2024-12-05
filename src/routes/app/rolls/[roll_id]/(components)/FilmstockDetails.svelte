12a0c6eb0639851d3c6deb2ac63a66093a9245d6
Tomas Vergara <tominacho@hotmail.com>
2024-12-05 11:39:14 -0300

    mod: separated filmstock information in other component

---
+++ src/routes/app/rolls/[roll_id]/(components)/FilmstockDetails.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Badge } from '$lib/components/ui/badge';
+
+	export let roll;
+</script>
+
+<div class="text-md mb-3 flex">
+	{roll.brand_name}
+	{roll.filmstock_name}
+	{#if roll.iso}
+		<Badge
+			variant="outline"
+			class="ml-2  {roll.iso < 250
+				? 'border-blue-600 bg-blue-200 text-blue-600'
+				: roll.iso < 450
+					? 'border-orange-600 bg-orange-200 text-orange-600'
+					: roll.is < 1650
+						? 'border-purple-600 bg-purple-200 text-purple-600'
+						: 'border-rose-600 bg-rose-200 text-rose-600'}">{roll.iso}iso</Badge
+		>
+	{/if}
+</div>
+<hr />
+<div class="mt-3 flex">
+	<div class="w-1/2">
+		<h3 class="font-semibold">format</h3>
+		<p>{roll.format_name}</p>
+	</div>
+	<div class="w-1/2">
+		<h3 class="font-semibold">filmtype</h3>
+		<p>{roll.filmtype_name}</p>
+	</div>
+</div>
--- src/routes/app/rolls/[roll_id]/+page.svelte
+++ src/routes/app/rolls/[roll_id]/+page.svelte
@@ -9,6 +9,7 @@
 	import PhotoUploadZone from './(components)/PhotoUploadZone.svelte';
 	import { invalidate } from '$app/navigation';
 	import { Upload } from 'lucide-svelte';
+	import FilmstockDetails from './(components)/FilmstockDetails.svelte';
 
 	const { data } = $props<{ data: PageData }>();
 	let { roll, photos } = $state(data);
@@ -48,33 +49,7 @@
 					<Accordion.Item value="item-1">
 						<Accordion.Trigger>filmstock information</Accordion.Trigger>
 						<Accordion.Content>
-							<div class="text-md mb-3 flex">
-								{roll.brand_name}
-								{roll.filmstock_name}
-								{#if roll.iso}
-									<Badge
-										variant="outline"
-										class="ml-2  {roll.iso < 250
-											? 'border-blue-600 bg-blue-200 text-blue-600'
-											: roll.iso < 450
-												? 'border-orange-600 bg-orange-200 text-orange-600'
-												: roll.is < 1650
-													? 'border-purple-600 bg-purple-200 text-purple-600'
-													: 'border-rose-600 bg-rose-200 text-rose-600'}">{roll.iso}iso</Badge
-									>
-								{/if}
-							</div>
-							<hr />
-							<div class="mt-3 flex">
-								<div class="w-1/2">
-									<h3 class="font-semibold">format</h3>
-									<p>{roll.format_name}</p>
-								</div>
-								<div class="w-1/2">
-									<h3 class="font-semibold">filmtype</h3>
-									<p>{roll.filmtype_name}</p>
-								</div>
-							</div>
+							<FilmstockDetails {roll} />
 						</Accordion.Content>
 					</Accordion.Item>
 					<Accordion.Item value="item-2">
