4e3352bc9700b7ec260675816a690b8a74458192
Tomas Vergara <tominacho@hotmail.com>
2024-12-04 22:19:09 -0300

    refactor: simplify image distribution logic and enhance column styling in MasonryGallery

--- src/lib/components/custom/MasonryGallery.svelte
+++ src/lib/components/custom/MasonryGallery.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import ImageWithLoader from './ImageWithLoader.svelte'; // Asegúrate de que la ruta sea correcta
+	import ImageWithLoader from './ImageWithLoader.svelte';
 
 	interface ImageData {
 		name: string;
@@ -14,30 +14,23 @@
 	let containerWidth = 0;
 	let containerRef: HTMLElement;
 
-	// Convertir el objeto photoUrls a un array de ImageData
 	$: images = Object.entries(photoUrls).map(([name, url]) => ({ name, url }));
 
-	// Función para calcular la altura total de una columna
 	function getColumnHeight(column: ImageData[]) {
-		return column.length; // Simplificado ya que ImageWithLoader manejará el aspecto ratio
+		return column.length;
 	}
 
-	// Función para distribuir imágenes
 	function distributeImages(imagesList: ImageData[]) {
 		columns = Array.from({ length: columnCount }, () => []);
 
 		imagesList.forEach((image) => {
-			// Encontrar la columna con menor altura
 			const columnHeights = columns.map(getColumnHeight);
 			const minHeight = Math.min(...columnHeights);
 			const targetColumnIndex = columnHeights.indexOf(minHeight);
-
-			// Agregar imagen a la columna más corta
 			columns[targetColumnIndex].push(image);
 		});
 	}
 
-	// Observar cambios en el tamaño del contenedor
 	function observeContainer() {
 		const resizeObserver = new ResizeObserver((entries) => {
 			containerWidth = entries[0].contentRect.width;
@@ -55,7 +48,6 @@
 		return observeContainer();
 	});
 
-	// Reaccionar a cambios en photoUrls
 	$: if (photoUrls) {
 		distributeImages(images);
 	}
@@ -66,9 +58,9 @@
 	class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
 >
 	{#each columns as column}
-		<div class="grid gap-4">
+		<div class="grid auto-rows-min gap-4">
 			{#each column as image}
-				<div class="overflow-hidden rounded-lg">
+				<div class="w-full overflow-hidden rounded-lg border">
 					<ImageWithLoader src={image.url} alt={image.name} />
 				</div>
 			{/each}
