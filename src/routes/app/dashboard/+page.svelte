7297967d5606c0d8c0155ee5eb095bf6da16d9a0
Tomas Vergara <tominacho@hotmail.com>
2024-12-07 13:22:18 -0300

    feat: enhance dashboard to support no rolls added yet

--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -37,14 +37,19 @@
 	<title>dashboard | fv</title>
 </svelte:head>
 
-<div class="container mx-auto px-4">
+<div class="container mx-auto h-full px-4">
 	<div
 		class="mb-7 flex h-min w-full items-center rounded-xl border bg-white p-3 px-3 shadow-sm dark:bg-zinc-950"
 	>
-		<Input class="w-64 rounded-xl" placeholder="search" bind:value={$filterStore.search} />
+		<Input
+			class="w-64 rounded-xl"
+			placeholder="search"
+			bind:value={$filterStore.search}
+			disabled={data.rolls.length === 0}
+		/>
 		<Popover.Root>
-			<Popover.Trigger>
-				<Button class="ml-2 rounded-xl" variant="outline">
+			<Popover.Trigger disabled={data.rolls.length === 0}>
+				<Button class="ml-2 rounded-xl" variant="outline" disabled={data.rolls.length === 0}>
 					<Filter class="mr-2 h-4 w-4" />filter
 				</Button>
 			</Popover.Trigger>
@@ -63,5 +68,39 @@
 			<Plus class="mr-2 h-4 w-4" />new roll
 		</Button>
 	</div>
-	<PhotoGrid rolls={filteredRolls} />
+	{#if data.rolls.length === 0}
+		<div class="flex h-full w-full flex-col items-center justify-center">
+			<img
+				class="hidden w-1/4 select-none dark:block"
+				src="/img/illustrations/add-white-img.png"
+				alt=""
+				srcset=""
+			/>
+			<img
+				class="w-1/4 select-none dark:hidden"
+				src="/img/illustrations/add-black-img.png"
+				alt=""
+				srcset=""
+			/>
+			<p class="text-center">you haven't created any rolls yet.</p>
+		</div>
+	{:else if filteredRolls.length === 0}
+		<div class="flex h-full w-full flex-col items-center justify-center">
+			<img
+				class="hidden w-1/4 select-none dark:block"
+				src="/img/illustrations/searching-white-img.png"
+				alt=""
+				srcset=""
+			/>
+			<img
+				class="w-1/4 select-none dark:hidden"
+				src="/img/illustrations/searching-black-img.png"
+				alt=""
+				srcset=""
+			/>
+			<p class="text-center">no rolls match the filters.</p>
+		</div>
+	{:else}
+		<PhotoGrid rolls={filteredRolls} />
+	{/if}
 </div>
